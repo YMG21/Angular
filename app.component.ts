6fb108a1d7b6407e6f329228dd84eba581427784
YMG21 <49517196+YMG21@users.noreply.github.com>
2021-03-17 12:02:03 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  title = 'ToDoApp';
+
+
+ getValue(value)
+ {
+   console.warn(value)
+ }
+
+}
